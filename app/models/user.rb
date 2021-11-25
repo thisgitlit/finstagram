896c3bc167fc67bd522549b4150e10f680f07b17
thisgitlit <marc.duarte@outlook.com>
2021-11-25 01:41:56 +0000

    W4D2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User <ActiveRecord::Base
     has_many :finstagram_posts
     has_many :comments
     has_many :likes 
+    validates :email, :username, uniqueness: true 
+    validates :email, :avatar_url, :username, :password, presence: true
 
 end 
